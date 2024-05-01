aa3df22cd902862489a4f74d3534a2faa174a4b0
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-04-30 23:16:20 -0300

    problem: add problem 1038

---
+++ academic/problems/1038/main.py
@@ -0,0 +1,25 @@
+"""
+### Sobre
+Com base na tabela abaixo, escreva um programa que leia o
+código de um item e a quantidade deste item. A seguir,
+calcule e mostre o valor da conta a pagar.
+
+| CODIGO |  PRECO  |
+| :----: | :-----: |
+| 1      | R$ 4.00 |
+| 2      | R$ 4.50 |
+| 3      | R$ 5.00 |
+| 4      | R$ 2.00 |
+| 5      | R$ 1.50 |
+
+
+### Entrada
+O arquivo de entrada contém dois valores inteiros correspondentes ao código e à quantidade de um item conforme tabela acima.
+
+### Saída
+O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser pago, com 2 casas após o ponto decimal.
+"""
+
+"""
+"""
+
