69f618a06f780754e4bbd030ddb7b8d6edc67d02
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-06-07 10:52:06 -0300

    typo(docstring): problem dosctring description

--- academic/problems/1038/main.py
+++ academic/problems/1038/main.py
@@ -1,8 +1,8 @@
 """
 ### Sobre
 Com base na tabela abaixo, escreva um programa que leia o
-código de um item e a quantidade deste item. A seguir,
-calcule e mostre o valor da conta a pagar.
+código de um item e a quantidade deste item.
+A seguir, calcule e mostre o valor da conta a pagar.
 
 | CODIGO |  PRECO  |
 | :----: | :-----: |
